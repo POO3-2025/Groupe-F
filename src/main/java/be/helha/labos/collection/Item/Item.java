8710462aeba47dfc68c13ce3ce0d9c165d384adc
LA237567-Rechache <la237567@student.helha.be>
2025-05-07 15:36:56 +0200

    javadoc Item

--- src/main/java/be/helha/labos/collection/Item/Item.java
+++ src/main/java/be/helha/labos/collection/Item/Item.java
@@ -2,25 +2,47 @@ package be.helha.labos.collection.Item;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import org.bson.types.ObjectId;
-//import be.helha.labos.collection.User;
+import be.helha.labos.collection.User;
 
-public class Item {
+/**
+ * Classe mère représentant un objet dans le jeu.
+ * Elle contient des informations sur l'objet, telles que son nom, son type, etc.
+ */
+public class Item
+{
+        /**
+         * Attributs de la classe Item
+         * @JsonProperty est utilisé pour indiquer que cet attribut doit être sérialisé/désérialisé avec ce nom
+         * @ObjectId est un identifiant unique généré par MongoDB
+         * @param name le nom de l'objet
+         * @param type le type de l'objet
+         * @param user l'utilisateur auquel appartient l'objet
+         */
         @JsonProperty("_id")
         protected ObjectId id;
         protected int idUser;
         protected String name;
         protected String type;
 
+        /**
+         * Constructeur vide
+         */
         public Item() {
             this.name = getName();
             this.id = new ObjectId();
             //this.idUser = getIdUser();
         }
 
+        /**
+         * ObjectIdWrapper est une classe interne utilisée pour encapsuler l'identifiant d'objet
+         */
         public static class ObjectIdWrapper {
             @JsonProperty("$oid")
             private String oid;
 
+            /**
+             * getOid et setOid sont des méthodes d'accès pour l'identifiant d'objet
+             */
             public String getOid() { return oid; }
             public void setOid(String oid) { this.oid = oid; }
 
@@ -30,23 +52,42 @@ public class Item {
             }
         }
 
+        /**
+        * getter pour l'identifiant d'objet
+        * @return
+         */
         public ObjectId getId() {
             return id;
         }
 
+        /**
+         * setter pour l'identifiant d'objet
+         * @param id
+         */
         public void setId(ObjectId id) {
             this.id = id;
         }
 
+        /**
+         * getter pour l'identifiant d'utilisateur
+         * @return
+         */
         public String getName() {
             return name;
         }
 
+        /**
+         * setter pour l'identifiant d'utilisateur
+         * @param idUser
+         */
         public void setName(String name) {
             this.name = name;
         }
 
-
+        /**
+         * getter pour le type d'objet
+         * @return
+         */
         @Override
         public String toString() {
             return "Item2{" +
